7cbdb0717b4a05a3bfab4163f72fefdd4bde1b2e
Rob Baynes <rbaynes@mit.edu>
2017-11-20 14:01:22 -0500

    basic open phenome schema

--- bigquery-setup/create_bigquery_dataset_and_schema.sh
+++ bigquery-setup/create_bigquery_dataset_and_schema.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-export GCLOUD_PROJECT=openag-cloud-v1
-export GOOGLE_APPLICATION_CREDENTIALS=../service_account.json
-export DATASET=test_dataset
+source gcloud_env.bash
+
+mk_schema(){
+  FILE=$1
+  FILE+="_schema.json"
+  bq mk -t $DATASET.$1 $FILE
+}
 
 # This next command should automatically authorize this computer to our
 # cloud project with the service_account.json credentials (did you remember to
@@ -13,11 +17,13 @@ gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS 
 # Make the dataset (check if it exists first)
 bq mk $DATASET
 if [ $? -eq 1 ]; then
-  echo "Exiting script."
+  echo "Exiting script, Does the dataset already exist?"
   exit 1
 fi
 
-# Create the entire schema
-#bq mk -t test_dataset.test_table schema.json
+# Create all tables in the schema
+mk_schema $EXP_TABLE 
+mk_schema $TRE_TABLE 
+mk_schema $VAL_TABLE 
+mk_schema $COM_TABLE 
 
-#bq load ds.new_tbl ./info.csv ./info_schema.json
---
+++ bigquery-setup/gcloud_env.bash
@@ -0,0 +1,9 @@
+# Meant to be sourced in our bash scripts
+
+export GCLOUD_PROJECT=openag-cloud-v1
+export GOOGLE_APPLICATION_CREDENTIALS=../service_account.json
+export DATASET=OpenPhenome
+export EXP_TABLE=exp
+export TRE_TABLE=treat
+export VAL_TABLE=val
+export COM_TABLE=com
