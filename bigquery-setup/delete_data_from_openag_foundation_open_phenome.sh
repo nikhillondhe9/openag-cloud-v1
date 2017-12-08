fd8e9881c9d271e7c54979926ab86a42d7815c8a
Rob Baynes <rbaynes@mit.edu>
2017-12-08 17:36:50 -0500

    handy script to clean up public data after I test the copy exp. script

---
+++ bigquery-setup/delete_data_from_openag_foundation_open_phenome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sh delete_data.sh openag_foundation_open_phenome  exp
+sh delete_data.sh openag_foundation_open_phenome  treat
+sh delete_data.sh openag_foundation_open_phenome  val
+sh delete_data.sh openag_foundation_open_phenome  com
+sh delete_data.sh openag_foundation_open_phenome  dev
+sh delete_data.sh openag_foundation_open_phenome  gcms
+sh delete_data.sh openag_foundation_open_phenome  mol
