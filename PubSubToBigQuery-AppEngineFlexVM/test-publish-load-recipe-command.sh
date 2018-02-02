fc205b5aaa00dadeac494a73a587303607375a5b
Rob Baynes <rbaynes@mit.edu>
2018-02-01 20:33:58 -0500

    tests

--- PubSubToBigQuery-AppEngineFlexVM/test-publish-load-recipe-command.sh
+++ PubSubToBigQuery-AppEngineFlexVM/test-publish-load-recipe-command.sh
@@ -5,9 +5,16 @@ if ! [ -z "${VIRTUAL_ENV}" ] ; then
     echo "deactivate"
 fi
 
-source pubsub_env/bin/activate
-source ../gcloud_env.bash
+if [[ -z "${TOP_DIR}" ]]; then
+  # gcloud_env.bash has not been sourced.
+  export TOP_DIR="${PWD}/.."
+  source $TOP_DIR/gcloud_env.bash
+fi
 
-#./test-publish-command.py --command LoadRecipeIntoVariable --arg1 "`cat recipe.json`"
+source pubsub_env/bin/activate
+
+# send base64 recipe?
+#./test-publish-command.py --command LoadRecipeIntoVariable --arg1 "`cat recipe.json`" --arg2 5000
+
 ./test-publish-command.py --command Status
 
