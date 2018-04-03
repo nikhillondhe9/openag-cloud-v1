e78056499032263b8e804a749541a88454f9ab75
Rob Baynes <rbaynes@mit.edu>
2018-04-03 16:01:33 -0400

    fixed

--- device-auth/run.sh
+++ device-auth/run.sh
@@ -18,12 +18,12 @@ GCLOUD_PROJECT=openag-v1
 
 source pyenv/bin/activate
 
-python dev-auth.py --fb_service_account $FB_SA \
-                   --region $GCLOUD_REGION \
-                   --iot_project $GCLOUD_PROJECT \
-                   --iot_service_account $IOT_SA \
-                   --registry $GCLOUD_DEV_REG \
-                   --user_email rbaynes@mit.edu \
-                   --verification_code $1
+python3 dev-auth.py --fb_service_account $FB_SA \
+                    --region $GCLOUD_REGION \
+                    --iot_project $GCLOUD_PROJECT \
+                    --iot_service_account $IOT_SA \
+                    --registry $GCLOUD_DEV_REG \
+                    --user_email rbaynes@mit.edu \
+                    --verification_code $1
 
 
