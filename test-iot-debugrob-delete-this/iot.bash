defa58f662b3a3dea449a8e0ffc8560ea089d184
Rob Baynes <rbaynes@mit.edu>
2018-04-05 14:14:15 -0400

    WIP

--- test-iot-debugrob-delete-this/iot.bash
+++
@@ -1,6 +0,0 @@
-# MUST use the central region / zone for beta IoT product.
-export GCLOUD_REGION=us-central1
-export GCLOUD_ZONE=us-central1-c
-
-# Kill this, since for IoT we won't deploy it.
-export GOOGLE_APPLICATION_CREDENTIALS=
