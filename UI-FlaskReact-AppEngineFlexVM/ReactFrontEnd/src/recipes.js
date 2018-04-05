e5cc8dcf0a15da2cfe2af549503725f399324288
Manvitha Ponnapati <manvithaponnapati@dhcp-18-111-19-180.dyn.MIT.EDU>
2018-04-05 14:42:43 -0400

    view recipe method

--- UI-FlaskReact-AppEngineFlexVM/ReactFrontEnd/src/recipes.js
+++ UI-FlaskReact-AppEngineFlexVM/ReactFrontEnd/src/recipes.js
@@ -2,14 +2,19 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './recipes.css';
 import {Cookies, withCookies} from "react-cookie";
-
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input} from 'reactstrap';
 
 class recipes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            all_recipes: []
-        }
+            all_recipes: [],
+            modal: false,
+            selected_recipe: {},
+            selected_recipe_json: {}
+        };
+
+        this.toggle = this.toggle.bind(this);
         this.getAllRecipes = this.getAllRecipes.bind(this)
     }
 
@@ -26,6 +31,30 @@ class recipes extends Component {
     componentDidMount() {
         console.log("Mouting component")
         this.getAllRecipes()
+    }
+
+
+    toggle(recipe, recipe_json) {
+
+        var json_html_append = [];
+        if (recipe !== undefined && recipe_json != undefined) {
+            recipe_json = JSON.parse(recipe_json)
+            Object.keys(recipe_json).forEach(function (key) {
+                if (key !== 'components' && key !== 'user_token' && key !== 'template_recipe_uuid') {
+                    json_html_append.push(<div className="row" key={key}>
+                        <div className="col-md-6"> {key}</div>
+                        <div className="col-md-6"> {recipe_json[key]}</div>
+                    </div>)
+                }
+            })
+            this.setState({
+                selected_recipe: recipe,
+                selected_recipe_json: json_html_append
+            })
+        }
+        this.setState({
+            modal: !this.state.modal
+        });
     }
 
     getAllRecipes() {
@@ -54,17 +83,26 @@ class recipes extends Component {
             });
     }
 
+
     render() {
         let listRecipes = <p>Loading</p>
         if (this.state.all_recipes.length > 0) {
             listRecipes = this.state.all_recipes.map((recipe) => {
+                let recipe_json_value = JSON.parse(recipe.recipe_json)
                 return <div className="col-md-3" key={recipe.recipe_uuid}>
                     <div className="card">
                         <div className="card-body">
                             <h5 className="card-title">{recipe.recipe_name}</h5>
                             <h6 className="card-subtitle mb-2 text-muted">{recipe.recipe_plant}</h6>
-                            {/*<p className="card-text">Use this template recipe to create your custom recipes</p>*/}
-                            <div onClick={this.editRecipe.bind(this,recipe.recipe_uuid)} id={recipe.recipe_uuid} className="card-link">Edit Recipe</div>
+                            <div className="card-text">
+                                <div onClick={this.editRecipe.bind(this, recipe.recipe_uuid)} id={recipe.recipe_uuid}
+                                     className="card-link">Edit Recipe
+                                </div>
+                                <div onClick={this.toggle.bind(this, recipe, recipe.recipe_json)} id={recipe.recipe_uuid}
+                                     >View Recipe
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -79,13 +117,31 @@ class recipes extends Component {
                                 <div className="card-body">
                                     <h5 className="card-title">Standard Recipe</h5>
                                     <h6 className="card-subtitle mb-2 text-muted"></h6>
-                                    <p className="card-text">Use this template recipe to create your custom recipes</p>
-                                    <div onClick={this.editRecipe.bind(this,'0')} className="card-link">Edit Recipe</div>
+                                    <div className="card-text">Use this template recipe to create your custom recipes </div>
+                                    <div className="card-text">
+                                         <div onClick={this.editRecipe.bind(this, '0')}
+                                        className="card-link">Edit Recipe
+                                 </div>
                                 </div>
+                                </div>
+
+
                             </div>
                         </div>
                         {listRecipes}
                     </div>
+
+                    <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                        <ModalHeader toggle={this.toggle}>Recipe Details</ModalHeader>
+                        <ModalBody>
+                            <h2> {this.state.selected_recipe.recipe_name}
+                                for {this.state.selected_recipe.recipe_plant} </h2>
+                            {this.state.selected_recipe_json}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="secondary" onClick={this.toggle}>Close</Button>
+                        </ModalFooter>
+                    </Modal>
                 </div>
             </Router>
 
