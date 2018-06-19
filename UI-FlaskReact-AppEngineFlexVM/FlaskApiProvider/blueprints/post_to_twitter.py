005fe65796e7be5dff0e659d9837816e4e75fc3c
Jason Chen <kbtpodifo@gmail.com>
2018-06-19 13:11:14 -0400

    Get user_uuid from token when posting to twitter
    It's more reliable because user_uuid might not always be set on the
    client.

--- UI-FlaskReact-AppEngineFlexVM/FlaskApiProvider/blueprints/post_to_twitter.py
+++ UI-FlaskReact-AppEngineFlexVM/FlaskApiProvider/blueprints/post_to_twitter.py
@@ -5,6 +5,7 @@ from flask import request
 
 from .utils.env_variables import *
 from .utils.response import success_response, error_response
+from .utils.auth import get_user_uuid_from_token
 
 posttwitter_bp = Blueprint('posttwitter_bp',__name__)
 
@@ -12,8 +13,17 @@ posttwitter_bp = Blueprint('posttwitter_bp',__name__)
 def posttwitter():
     received_form_response = json.loads(request.data.decode('utf-8'))
     current_date = datetime.utcnow()
-    user_uuid = received_form_response.get("user_uuid", "Error")
-    api.update_status('Food computer status for %s on %s' % (user_uuid, str(current_date)))
+    user_token = received_form_response.get("user_token")
+
+    user_uuid = get_user_uuid_from_token(user_token)
+    if user_uuid is None:
+        return error_response(
+            message="Invalid token. Unauthorized."
+        )
+
+    api.update_status(
+        'Food computer status for {} on {}'.format(user_uuid, current_date)
+    )
 
     return success_response(
         message="success"
