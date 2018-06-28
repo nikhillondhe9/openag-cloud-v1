a6b94aa986ab59f0a953b3b94f547cc5664087e7
Manvitha Ponnapati <ManvithaPonnapati@users.noreply.github.com>
2018-06-28 15:53:55 -0400

    push wip changes for new recipe

--- UI-FlaskReact-AppEngineFlexVM/FlaskApiProvider/blueprints/submit_recipe.py
+++ UI-FlaskReact-AppEngineFlexVM/FlaskApiProvider/blueprints/submit_recipe.py
@@ -11,6 +11,18 @@ from datetime import datetime, timedelta
 submit_recipe_bp = Blueprint('submit_recipe', __name__)
 
 
+def get_existing_recipes(recipe_key):
+    spectrum_json = {}
+    if(recipe_key == "flat"):
+        spectrum_json = {"400-449": 16.67, "450-499": 16.67, "500-549": 16.67, "550-559": 16.67, "600-649": 16.67, "650-699": 16.67}
+    if (recipe_key == "low_end"):
+        spectrum_json = {"400-449": 50, "450-499": 50, "500-549": 0, "550-559": 0, "600-649": 0, "650-699": 0},
+    if (recipe_key == "mid_end"):
+        spectrum_json = {"400-449": 0, "450-499": 0, "500-549": 50, "550-559": 50, "600-649": 0, "650-699": 0}
+    if (recipe_key == "high_end"):
+        spectrum_json = {"400-449": 0, "450-499": 0, "500-549": 0, "550-559": 0, "600-649": 50, "650-699": 50}
+    return spectrum_json
+
 # ------------------------------------------------------------------------------
 # Handle Change to a recipe running on a device
 @submit_recipe_bp.route('/api/submit_recipe/', methods=['GET', 'POST'])
@@ -73,33 +85,23 @@ def submit_recipe():
         "name": "Shallow Water Culture",
         "uuid": str(uuid.uuid4())
     }]
+    standard_day_led_spectrum = get_existing_recipes(recipe_key=recipe_state.get("led_panel_dac5578", {}).get("on_selected_spectrum",""))
+    standard_night_led_spectrum = get_existing_recipes(recipe_key=recipe_state.get("led_panel_dac5578", {}).get("off_selected_spectrum", ""))
+    off_illumination_distance = get_existing_recipes(recipe_key=recipe_state.get("led_panel_dac5578", {}).get("off_illumination_distance", 5))
+    on_illumination_distance = get_existing_recipes(recipe_key=recipe_state.get("led_panel_dac5578", {}).get("on_illumination_distance", 5))
 
     recipe_format["environments"]["standard_day"] = {
         "name": "Standard Day",
-        "light_spectrum_nm_percent": {"400-449": float(recipe_state.get("led_panel_dac5578", {}).get("on_red", 0)),
-                                      "450-499": float(recipe_state.get("led_panel_dac5578", {}).get("on_blue", 0)),
-                                      "500-549": float(recipe_state.get("led_panel_dac5578", {}).get("on_green", 0)),
-                                      "550-599": float(recipe_state.get("led_panel_dac5578", {}).get("on_far_red", 0)),
-                                      "600-649": float(
-                                          recipe_state.get("led_panel_dac5578", {}).get("on_warm_white", 0)),
-                                      "650-699": float(
-                                          recipe_state.get("led_panel_dac5578", {}).get("on_cool_white", 0))},
+        "light_spectrum_nm_percent": standard_day_led_spectrum,
         "light_intensity_watts": 100,
-        "light_illumination_distance_cm": recipe_state.get("led_panel_dac5578", {}).get("on_illumination_distance", 5),
+        "light_illumination_distance_cm": on_illumination_distance,
         "air_temperature_celcius": 22
     }
     recipe_format["environments"]["standard_night"] = {
         "name": "Standard Night",
-        "light_spectrum_nm_percent": {"400-449": float(recipe_state.get("led_panel_dac5578", {}).get("off_red", 0)),
-                                      "450-499": float(recipe_state.get("led_panel_dac5578", {}).get("off_blue", 0)),
-                                      "500-549": float(recipe_state.get("led_panel_dac5578", {}).get("off_green", 0)),
-                                      "550-599": float(recipe_state.get("led_panel_dac5578", {}).get("off_far_red", 0)),
-                                      "600-649": float(
-                                          recipe_state.get("led_panel_dac5578", {}).get("off_warm_white", 0)),
-                                      "650-699": float(
-                                          recipe_state.get("led_panel_dac5578", {}).get("off_cool_white", 0))},
+        "light_spectrum_nm_percent": standard_night_led_spectrum,
         "light_intensity_watts": 100,
-        "light_illumination_distance_cm": recipe_state.get("led_panel_dac5578", {}).get("off_illumination_distance", 5),
+        "light_illumination_distance_cm":off_illumination_distance ,
         "air_temperature_celcius": 22
     }
     recipe_format["environments"]["cold_day"] = {
