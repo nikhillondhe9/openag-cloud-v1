5a57f9ca562b1ffb18cdae7f0886093c88bc0854
Jason Chen <kbtpodifo@gmail.com>
2018-07-27 12:30:00 -0400

    Image timelapse handle switching devices
    Also, it shows a grey placeholder when there aren't images.

--- UI-FlaskReact-AppEngineFlexVM/ReactFrontEnd/src/js/components/image_timelapse.js
+++ UI-FlaskReact-AppEngineFlexVM/ReactFrontEnd/src/js/components/image_timelapse.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import placeholder from '../../images/no-image.png';
 
 /**
  * TimelapseImages
@@ -11,26 +12,55 @@ import React from 'react';
 export class ImageTimelapse extends React.PureComponent {
 
     state = {
-        index: 0
+        index: 0,
+        disabled: false
     }
 
     onSliderChange = (e) => {
         this.setState({index: e.target.value});
     }
 
+    componentWillReceiveProps = (nextProps) => {
+        // This resets the slider so that it doesn't try and
+        // access an out of range array index when switching
+        // sources.
+        if (!nextProps.images) {
+            this.setState({disabled: true});
+        } else {
+            this.setState({disabled: false});
+        }
+        this.setState({index: 0});
+    }
+
     render() {
+        if (!this.state.disabled) {
+            return (
+                <React.Fragment>
+                    <img
+                        src={this.props.images[this.state.index]}
+                        className={this.props.imageClass} />
+                    <input
+                        className={this.props.inputClass}
+                        type="range"
+                        min="0"
+                        value={this.state.index}
+                        max={this.props.images.length - 1}
+                        onChange={this.onSliderChange} />
+                </React.Fragment>
+            );
+        }
         return (
             <React.Fragment>
                 <img
-                    src={this.props.images[this.state.index]}
+                    src={placeholder}
                     className={this.props.imageClass} />
                 <input
                     className={this.props.inputClass}
                     type="range"
-                    defaultValue="0"
+                    value={this.state.index}
                     min="0"
-                    max={this.props.images.length - 1}
-                    onChange={this.onSliderChange} />
+                    max="1"
+                    disabled />
             </React.Fragment>
         );
     }
