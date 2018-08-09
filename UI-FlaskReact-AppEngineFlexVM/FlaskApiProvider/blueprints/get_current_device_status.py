9e8a056cad88769d838b59484bae8d72e61b2cee
Manvitha Ponnapati <ManvithaPonnapati@users.noreply.github.com>
2018-08-09 14:54:31 -0400

    status panel

--- UI-FlaskReact-AppEngineFlexVM/FlaskApiProvider/blueprints/get_current_device_status.py
+++ UI-FlaskReact-AppEngineFlexVM/FlaskApiProvider/blueprints/get_current_device_status.py
@@ -26,25 +26,32 @@ def get_current_device_status():
 
     device_data = get_device_data_from_DS(device_uuid)
 
-    result_json = {}
-    timestamp = device_data.get("timestamp").decode()
-    timenow = str(datetime.now())
-    fmt1 = '%Y-%m-%d %H:%M:%S.%f'
-    fmt2 = '%Y-%m-%dT%H:%M:%SZ'
+    result_json = {
+        "progress":0.0,
+        "age_in_days":0,
+        "wifi_status":"N/A for this device",
+        "current_temp":"N/A for this device"
+    }
+    if device_data is not None:
+        timestamp = device_data.get("timestamp").decode()
+        timenow = str(datetime.now())
+        fmt1 = '%Y-%m-%d %H:%M:%S.%f'
+        fmt2 = '%Y-%m-%dT%H:%M:%SZ'
 
-    t1 = datetime.strptime(timenow, fmt1)
-    t2 = datetime.strptime(timestamp, fmt2)
-    _,time_minutes,_ = convert_timedelta(t1-t2)
-    if time_minutes > 5:
-        wifi_status = "Disconnected"
-    else:
-        wifi_status = "Connected"
+        t1 = datetime.strptime(timenow, fmt1)
+        t2 = datetime.strptime(timestamp, fmt2)
 
-    result_json["current_temp"] = "%s C" %((device_data["air_temp"]).decode())
+        _,time_minutes,_ = convert_timedelta(t1-t2)
+        if time_minutes > 5:
+            wifi_status = "Disconnected"
+        else:
+            wifi_status = "Connected"
 
-    result_json["progress"] = int(round(float(device_data.get("percent_complete") if device_data.get("percent_complete") else "0.0"))*100.0)
-    result_json["wifi_status"] = wifi_status
-    result_json["age_in_days"] = device_data.get("time_elapsed","")
+        result_json["current_temp"] = "%s C" %((device_data["air_temp"]).decode())
+        result_json["progress"] = int(round(float(device_data.get("percent_complete") if device_data.get("percent_complete") else "0.0"))*100.0)
+        result_json["wifi_status"] = wifi_status
+        result_json["age_in_days"] = device_data.get("time_elapsed","")
+
     return success_response(
         results=result_json
     )
