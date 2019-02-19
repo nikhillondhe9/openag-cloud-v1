e0086daecf51c8534f8ab1601311347dea63810e
Rob Baynes <rbaynes@media.mit.edu>
2019-02-19 09:05:48 -0500

    check for py3.6 and use new way of creating virtual env.

--- UI-FlaskReact-AppEngineFlexVM/one_time_setup_for_local_development.sh
+++ UI-FlaskReact-AppEngineFlexVM/one_time_setup_for_local_development.sh
@@ -11,9 +11,29 @@ if ! [ -z "${VIRTUAL_ENV}" ] ; then
     deactivate
 fi
 
+# Install on linux or darwin operating system
+if [[ "$OSTYPE" == "linux"* || "$OSTYPE" == "darwin"* ]]; then
+
+    # Check if python (3.6) is installed
+    INSTALL_PATH=`which python3.6`
+    if [[ ! -f "$INSTALL_PATH" ]]; then
+        echo "Error: python3.6 is not installed, please go install it."
+        exit 1
+    fi
+
+# Invalid operating system
+else
+  echo "Error: unsupported operating system: $OSTYPE"
+  exit 1
+fi
+
+
 rm -fr $DIR/pyenv $DIR/FlaskApiProvider/lib $DIR/FlaskApiProvider/__pycache__  $DIR/ReactFrontEnd/node_modules
 
-virtualenv --python python3 pyenv
+# Create virtual environment
+python3.6 -m venv $DIR/pyenv
+
+# Activate the py virtual env
 source $DIR/pyenv/bin/activate
 
 cd $DIR/FlaskApiProvider
